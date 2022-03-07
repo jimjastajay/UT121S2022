11bc7b6eb989b2d605672328e93dff32a2ece26d
Shawn Pierre <shawn@origaminc.com>
2022-03-07 12:55:05 -0500

    week 6 sounds and coroutines

---
+++ Assets/Week6/Scripts/MyKeyboardKey.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MyKeyboardKey : MonoBehaviour {
+
+    public enum Accidental { Natural, Sharp }
+    public Accidental acc = Accidental.Natural;
+
+    public AudioSource audioSource;
+    public KeyCode keyboardLetter;
+    public AudioClip audioClip;
+
+    float basePitch = 1.0f;
+    float diff = 1.05946f - 1.0f;
+
+    private void Start() {
+        switch (acc) {
+            case Accidental.Sharp:
+                basePitch = 1.0f + diff;
+                break;
+            default:
+                basePitch = 1.0f;
+                break;
+        }
+    }
+
+    void PlayKey() {
+        audioSource.PlayOneShot(audioClip);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(keyboardLetter)) {
+
+            if (Input.GetKey(KeyCode.LeftShift)) {
+                audioSource.pitch = basePitch * 2.0f;
+            }
+            else {
+                audioSource.pitch = basePitch;
+            }
+
+
+            PlayKey();
+        }
+    }
+
+}
